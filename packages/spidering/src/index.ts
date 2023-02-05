fb7064966d1099dc208c95a4b5a1295b9a2effab
Casaubon <netzach.yoshida@gmail.com>
2023-02-05 18:19:27 +0900

    refactor: 💡 small parts

--- packages/spidering/src/index.ts
+++ packages/spidering/src/index.ts
@@ -1,5 +1,7 @@
 import got from "got";
 import { JSDOM } from "jsdom";
+import type HTMLCollectionOf from "jsdom";
+import type Element from "jsdom";
 
 export const response = {
   hello: "world",
@@ -21,8 +23,57 @@ export type ScheduleContent = {
   txt?: ScheduleTxt[];
 };
 
+/**
+ * WIP
+ */
+export const domCalc = (
+  elements: HTMLCollectionOf<Element>,
+  doElement: (element: Element) => void,
+): void => {
+  if (elements.length > 0) {
+    for (const element of elements) {
+      doElement(element);
+    }
+  }
+};
+
+export const makePureTitle = (
+  title: HTMLParagraphElement,
+  discriptionList: string[],
+  titleList: string[],
+  honDate?: Element,
+  honDateList?: string[],
+) => {
+  const discriptionDom = title.getElementsByTagName("small");
+  for (const discription of discriptionDom) {
+    console.log(`discription: ${discription.textContent}`);
+    const pureDiscription = discription.textContent;
+    if (pureDiscription) {
+      discriptionList.push(pureDiscription);
+    }
+    if (honDate && honDateList) {
+      const pureHonDate = honDate.textContent;
+      if (pureHonDate) {
+        honDateList.push(pureHonDate);
+      }
+      honDate.remove();
+    }
+    discription.remove();
+    const pureTitle = title.textContent?.replace(/^\s+/, "");
+    console.log(`pure title: ${pureTitle}`);
+    if (pureTitle) {
+      titleList.push(pureTitle);
+    }
+  }
+};
+
 const init = async () => {
   const data = await got.get("https://www.shin-bungeiza.com/schedule.html");
+
+  let imgList: string[] = [];
+  let honDateList: string[] = [];
+  let discriptionList: string[] = [];
+  let titleList: string[] = [];
 
   const dom = new JSDOM(data.body, {
     runScripts: "dangerously",
@@ -35,25 +86,23 @@ const init = async () => {
     const schedulePrograms = element.getElementsByClassName("schedule-program");
     for (const img of images) {
       console.log(`https://www.shin-bungeiza.com${img.src}`);
+      imgList.push(`https://www.shin-bungeiza.com${img.src}`);
     }
     for (const h2 of h2collections) {
       console.log(h2.textContent);
+      // TODO: convert h2.textContent to date type
     }
     for (const scheduleProgram of schedulePrograms) {
       const titles = scheduleProgram.getElementsByTagName("p");
       for (const title of titles) {
-        const honDateList = title.getElementsByClassName("hon-date");
-        for (const honDate of honDateList) {
-          console.log(honDate.textContent);
-          // make pure title
-          const discriptionList = title.getElementsByTagName("small");
-          for (const discription of discriptionList) {
-            console.log(discription.textContent);
-            honDate.remove();
-            discription.remove();
-            const pureTitle = title.textContent?.replace(/^\s+/, "");
-            console.log(pureTitle);
+        const honDateDom = title.getElementsByClassName("hon-date");
+        if (honDateDom.length > 0) {
+          for (const honDate of honDateDom) {
+            console.log(honDate.textContent);
+            makePureTitle(title, discriptionList, titleList, honDate, honDateList);
           }
+        } else {
+          makePureTitle(title, discriptionList, titleList);
         }
       }
       const timelist = scheduleProgram.getElementsByTagName("li");
@@ -61,6 +110,7 @@ const init = async () => {
         console.log(time.textContent);
       }
     }
+    console.log("\n\n");
   }
 };
 
