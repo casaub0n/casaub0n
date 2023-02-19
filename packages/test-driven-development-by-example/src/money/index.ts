01408042983c64624b2d185f76f38ffd07d982ae
Casaubon <netzach.yoshida@gmail.com>
2023-02-20 00:02:42 +0900

    test: 💍 compare currency that is dollar and franc

--- packages/test-driven-development-by-example/src/money/index.ts
+++ packages/test-driven-development-by-example/src/money/index.ts
@@ -1,9 +1,11 @@
 export class Money {
   protected amount: number;
+
   constructor(amount: number) {
     this.amount = amount;
   }
+
   public equals(object: Money): boolean {
-    return this.amount === object.amount;
+    return this.amount === object.amount && this.constructor.name === object.constructor.name;
   }
 }
