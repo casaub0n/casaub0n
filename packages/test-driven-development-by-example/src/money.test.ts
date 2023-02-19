c805208e08d87e1310819086c6e9c043f8f4ab78
Casaubon <netzach.yoshida@gmail.com>
2023-02-19 23:27:19 +0900

    feat: 🎸 add franc

--- packages/test-driven-development-by-example/src/money.test.ts
+++ packages/test-driven-development-by-example/src/money.test.ts
@@ -1,12 +1,19 @@
 import { Dollar } from "./dollar";
+import { Franc } from "./franc";
 
-test("$5 * 2 = $10", () => {
+test("multiplication", () => {
   const five = new Dollar(5);
   expect(five.times(2)).toEqual(new Dollar(10));
   expect(five.times(3)).toEqual(new Dollar(15));
 });
 
-test("equals methods", () => {
+test("equality", () => {
   expect(new Dollar(5).equals(new Dollar(5))).toBe(true);
   expect(new Dollar(5).equals(new Dollar(6))).toBe(false);
 });
+
+test("Franc Multiplication", () => {
+  const five = new Franc(5);
+  expect(five.times(2)).toEqual(new Franc(10));
+  expect(five.times(3)).toEqual(new Franc(15));
+})
