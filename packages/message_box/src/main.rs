2bfdb2520589b80fb372d3101b7ee9e322ddd3ad
Casaubon <netzach.yoshida@gmail.com>
2023-03-14 21:26:46 +0900

    feat: 🎸 WinRT example for message box

---
+++ packages/message_box/src/main.rs
@@ -0,0 +1,11 @@
+use windows::{
+    core::*,
+    Win32::UI::WindowsAndMessaging::*
+};
+
+fn main() {
+    unsafe {
+        MessageBoxA(None, s!("Ansi"), s!("World"), MB_OK);
+        MessageBoxW(None, w!("Wide"), w!("World"), MB_OK);
+    }
+}
