e98443a31747afa4d4fe7547efa0c21993b5c34b
Casaubon <netzach.yoshida@gmail.com>
2023-06-28 17:41:02 +0900

    fix: 🐛 tests

--- packages/mdlink/src/mdlink.test.ts
+++ packages/mdlink/src/mdlink.test.ts
@@ -1,9 +1,12 @@
 import puppeteer from "puppeteer";
+
 import { markdownLink } from "./markdownLink";
 
 describe("Google", () => {
-  it('markdown link', async () => {
-    const browser = await puppeteer.launch();
+  it("markdown link", async () => {
+    const browser = await puppeteer.launch({
+      headless: "new",
+    });
     const page = await browser.newPage();
     await page.goto("https://google.com");
     const title = await page.title();
