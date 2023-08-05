6409d38fcb726521529d0ee08191565a8f01b1aa
Casaubon <netzach.yoshida@gmail.com>
2023-08-06 01:47:53 +0900

    feat: 🎸 add japanese fonts (#534)

---
+++ apps/vitalform/src/app/fonts.ts
@@ -0,0 +1,11 @@
+import { Cherry_Bomb_One, Inter } from "next/font/google";
+
+export const inter = Inter({
+  subsets: ["latin"],
+  display: "swap",
+});
+
+export const cherry_bomb_one = Cherry_Bomb_One({
+  weight: "400",
+  preload: false,
+});
