5268160ae0398198533e71d15ad01ffa52abd24b
Casaubon <netzach.yoshida@gmail.com>
2023-08-06 01:44:56 +0900

    feat: 🎸 add japanese fonts

--- apps/vitalform/src/components/templates/Top/Top.tsx
+++ apps/vitalform/src/components/templates/Top/Top.tsx
@@ -7,6 +7,8 @@ import { MyAvatar } from "@/components/atoms/MyAvatar";
 
 import css from "@/utils/pandaLoader";
 
+import { cherry_bomb_one } from "@/app/fonts";
+
 type Props = ComponentPropsWithoutRef<"main">;
 
 export const Top: FC<Props> = ({ className, ...props }) => {
@@ -14,6 +16,7 @@ export const Top: FC<Props> = ({ className, ...props }) => {
     <main className={clsx(className)} {...props}>
       <section className={clsx(css({ display: "grid", placeItems: "center" }))}>
         <MyAvatar className={clsx(css({ paddingTop: "10" }))} />
+        <h1 className={clsx(cherry_bomb_one.className, css({ paddingTop: "10" }))}>まじでやる</h1>
         <div className={clsx(css({ paddingTop: "10" }))}>
           <Tweet id='1671802628051984384' />
         </div>
