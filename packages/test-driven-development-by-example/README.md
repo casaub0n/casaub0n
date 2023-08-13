ddef9b45cea022aee3b6fcfb5c9d8a0c74d298dc
Casaubon <netzach.yoshida@gmail.com>
2023-08-14 03:29:03 +0900

    Use Node.js testing runner (#562)
    * feat: 🎸 node.js testing runner
    
    remove jest and install the loader for TyepeScript

--- packages/test-driven-development-by-example/README.md
+++ packages/test-driven-development-by-example/README.md
@@ -1,11 +1,32 @@
 # Test Driven Development By Example
+
 [area: test-driven-by-example](https://github.com/casaub0n/casaub0n/labels/area%3A%20test-driven-by-example "use this label in GitHub")
 
 - the author: Kent Beck
 
 This project was implemented example codes by TypeScript.
 
+## Getting Started
+
+```console
+pnpm install
+pnpm test
+```
+
+## Test Environment
+
+I followed this link 👉
+[How to run the Node built-in testrunner for TypeScript files inside a specific directory? · Issue #3902 · nodejs/help](https://github.com/nodejs/help/issues/3902#issuecomment-1307124174)
+
+This LTS version's Node.js shows the warning:
+`ExperimentalWarning: Custom ESM Loaders is an experimental feature and might change at any time`
+
+### Motivation
+
+Use standard functions by Node.js
+
 ## Conclusion
+
 Test Driven Development By Example has some issues, but that's still useful book.
 
 Kent Beck shows the way to implement the code from his idea. Some implementation looks winding road, but test case is very clear.
@@ -13,18 +34,22 @@ Kent Beck shows the way to implement the code from his idea. Some implementation
 Currently we have more choice for TDD that is type system, integration test and maybe Model Checking.
 
 ## Circular Reference
+
 In money package, each modules uses Factory method, but it occurs the technical issue about circular reference.
 In addition, the actual error message is `TypeError: Class extends value undefined is not a constructor or null`
 
 ### Solution:
+
 This solution looks good 👉 https://jestjs.io/ja/docs/ecmascript-modules
 
 ## equals method
-As Java, when we compare strings, we use `equals()` method because `String` is Object. But as TypeScript, it must be `===` operator because `string` type is primitive data type. 
+
+As Java, when we compare strings, we use `equals()` method because `String` is Object. But as TypeScript, it must be `===` operator because `string` type is primitive data type.
 
 `HashMap` is Object in Java that means Key Object must has equals and hashcode method. Lately that must implement `Comarable` like this:
 
 https://interprism.hatenablog.com/entry/2014/04/04/125444
+
 ```Java
 package jp.co.interprism;
 
@@ -60,11 +85,13 @@ public class HashCompareEqualKey implements Comparable<HashCompareEqualKey> {
 In this project, I used tuple because it is easy as TypeScript.
 
 ## Member variable
+
 In TypeScript Class, member variable can't use method name.
 
 If you use member variable, you must specify `this` keyword because of distinguishing memeber variable from normal variable.
 
 ## Down casting
+
 I can't make sense that arg's type is often Object.
 
 Don't do down casting.
--- packages/test-driven-development-by-example/src/money.test.ts
+++ packages/test-driven-development-by-example/src/money.test.ts
@@ -1,20 +1,23 @@
-import { Money, Bank, Sum } from "./money";
+import { strict as assert } from "assert";
+import { test } from "node:test";
+
+import { Bank, Money, Sum } from "./money";
 
 test("multiplication", () => {
   const five = Money.doller(5);
-  expect(five.times(2)).toEqual(Money.doller(10));
-  expect(five.times(3)).toEqual(Money.doller(15));
+  assert.deepStrictEqual(five.times(2), Money.doller(10));
+  assert.deepStrictEqual(five.times(3), Money.doller(15));
 });
 
 test("equality", () => {
-  expect(Money.doller(5).equals(Money.doller(5))).toBe(true);
-  expect(Money.doller(5).equals(Money.doller(6))).toBe(false);
-  expect(Money.doller(5).equals(Money.franc(5))).toBe(false);
+  assert.deepStrictEqual(Money.doller(5), Money.doller(5));
+  assert.notDeepStrictEqual(Money.doller(5), Money.doller(6));
+  assert.notDeepStrictEqual(Money.doller(5), Money.franc(5));
 });
 
 test("currency", () => {
-  expect(Money.doller(1).currency).toBe("USD");
-  expect(Money.franc(1).currency).toBe("CHF");
+  assert.deepStrictEqual(Money.doller(1).currency, "USD");
+  assert.deepStrictEqual(Money.franc(1).currency, "CHF");
 });
 
 test("simple addition", () => {
@@ -22,38 +25,38 @@ test("simple addition", () => {
   const sum = five.plus(five);
   const bank = new Bank();
   const reduced = bank.reduce(sum, "USD");
-  expect(reduced).toEqual(Money.doller(10));
+  assert.deepStrictEqual(reduced, Money.doller(10));
 });
 
 test("plus return sum", () => {
   const five = Money.doller(5);
   const sum = five.plus(five);
-  expect(five).toBe(sum.augend);
-  expect(five).toBe(sum.addend);
+  assert.deepStrictEqual(five, sum.augend);
+  assert.deepStrictEqual(five, sum.addend);
 });
 
 test("reduce sum", () => {
   const sum = new Sum(Money.doller(3), Money.doller(4));
   const bank = new Bank();
   const result = bank.reduce(sum, "USD");
-  expect(result).toEqual(Money.doller(7));
+  assert.deepStrictEqual(result, Money.doller(7));
 });
 
 test("reduce money", () => {
   const bank = new Bank();
   const result = bank.reduce(Money.doller(1), "USD");
-  expect(result).toEqual(Money.doller(1));
+  assert.deepStrictEqual(result, Money.doller(1));
 });
 
 test("reduce Money different Currency", () => {
   const bank = new Bank();
   bank.addRate("CHF", "USD", 2);
   const result = bank.reduce(Money.franc(2), "USD");
-  expect(result).toEqual(Money.doller(1));
+  assert.deepStrictEqual(result, Money.doller(1));
 });
 
 test("identy rate", () => {
-  expect(new Bank().rate("USD", "USD")).toBe(1);
+  assert.deepStrictEqual(new Bank().rate("USD", "USD"), 1);
 });
 
 test("mixed addition", () => {
@@ -62,7 +65,7 @@ test("mixed addition", () => {
   const bank = new Bank();
   bank.addRate("CHF", "USD", 2);
   const result = bank.reduce(fiveBucks.plus(tenFrancs), "USD");
-  expect(result).toEqual(Money.doller(10));
+  assert.deepStrictEqual(result, Money.doller(10));
 });
 
 test("sum plus money", () => {
@@ -72,7 +75,7 @@ test("sum plus money", () => {
   bank.addRate("CHF", "USD", 2);
   const sum = new Sum(fiveBucks, tenFrancs).plus(fiveBucks);
   const result = bank.reduce(sum, "USD");
-  expect(result).toEqual(Money.doller(15));
+  assert.deepStrictEqual(result, Money.doller(15));
 });
 
 test("sum times", () => {
@@ -82,5 +85,5 @@ test("sum times", () => {
   bank.addRate("CHF", "USD", 2);
   const sum = new Sum(fiveBucks, tesFrancs).times(2);
   const result = bank.reduce(sum, "USD");
-  expect(result).toEqual(Money.doller(20));
+  assert.deepStrictEqual(result, Money.doller(20));
 });
