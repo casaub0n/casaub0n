eb48449c71ed8db861a15b63861f425743d10200
Casaubon <netzach.yoshida@gmail.com>
2023-08-15 02:43:20 +0900

    fix: 🐛 bundler (#571)

---
+++ packages/mdlink/README.md
@@ -0,0 +1,4 @@
+# bundler design
+
+- [Mark all node_modules as external · Issue #289 · egoist/tsup](https://github.com/egoist/tsup/issues/289#issuecomment-1382492040)
+- [error: Invalid define: --define:process.env.NODE_ENV=development · Issue #44 · evanw/esbuild](https://github.com/evanw/esbuild/issues/44#issuecomment-606719394)
--- packages/mdlink/src/mdlink.test.ts
+++ packages/mdlink/src/mdlink.test.ts
@@ -4,7 +4,7 @@ import puppeteer from "puppeteer";
 
 import { markdownLink } from "./markdownLink";
 
-describe("Google", () => {
+describe("Example Domain", () => {
   it("markdown link", async () => {
     console.log(`chrome path: ${env.CHROME}`);
     const browser = await puppeteer.launch({
@@ -12,9 +12,11 @@ describe("Google", () => {
       executablePath: env.CHROME,
     });
     const page = await browser.newPage();
-    await page.goto("https://google.com");
+    await page.goto("https://example.com/");
     const title = await page.title();
-    expect(markdownLink(title, "https://google.com")).toBe("[Google](https://google.com)");
+    const uri = page.url();
+    console.log(markdownLink(title, uri));
+    expect(markdownLink(title, uri)).toBe("[Example Domain](https://example.com/)");
     await browser.close();
   });
 });
