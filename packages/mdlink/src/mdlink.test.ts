4269f368f4cc81215d406245074f0f8e6e9b3a3c
Casaubon <netzach.yoshida@gmail.com>
2023-08-14 22:22:29 +0900

    feat: 🎸 chrome testing (#570)

--- packages/mdlink/src/mdlink.test.ts
+++ packages/mdlink/src/mdlink.test.ts
@@ -1,5 +1,4 @@
-// import { env } from "node:process";
-import "dotenv/config";
+import { env } from "node:process";
 
 import puppeteer from "puppeteer";
 
@@ -7,12 +6,10 @@ import { markdownLink } from "./markdownLink";
 
 describe("Google", () => {
   it("markdown link", async () => {
-    // eslint-disable-next-line turbo/no-undeclared-env-vars
-    console.log(`chrome path: ${process.env.CHROME}`);
+    console.log(`chrome path: ${env.CHROME}`);
     const browser = await puppeteer.launch({
       headless: "new",
-      // eslint-disable-next-line turbo/no-undeclared-env-vars
-      executablePath: process.env.CHROME,
+      executablePath: env.CHROME,
     });
     const page = await browser.newPage();
     await page.goto("https://google.com");
