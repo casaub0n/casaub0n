6fcd211be5607b8b0eb84c09ae43c8e33c0ad379
casaub0n <netzach.yoshida@gmail.com>
2023-10-11 12:25:17 +0900

    feat: 🎸 more metadata

--- apps/casaub0n-page/src/app/layout.tsx
+++ apps/casaub0n-page/src/app/layout.tsx
@@ -13,16 +13,24 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = layoutMetadata;
 
+/**
+ * @see https://nextjs.org/docs/app/building-your-application/optimizing/metadata#json-ld
+ */
 const jsonLd: WithContext<Article> = {
   "@type": "BlogPosting",
   "@context": "https://schema.org",
+  "headline": "My web site",
   "name": "casaub0n web page",
+  "image": [
+    "https://casaub0n-page.vercel.app/_next/image?url=%2Fimages%2Fme.jpg&w=384&q=75"
+  ],
+  "url": "https://casaub0n-page.vercel.app",
   "author": {
     "@type": "Person",
     "name": "casaub0n"
   },
   "datePublished": "2023-10-03",
-  "description": "This page is my web site"
+  "description": "This page is my web site that shows my favorite own tweet"
 }
 
 export default function RootLayout({ children }: { children: ReactNode }) {
