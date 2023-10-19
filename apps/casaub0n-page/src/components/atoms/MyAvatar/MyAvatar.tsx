a4e40cd6f75a3ba31334b6477618690360c9733f
casaub0n <netzach.yoshida@gmail.com>
2023-10-19 20:12:38 +0900

    refactor: 💡 add comment and margin

--- apps/casaub0n-page/src/components/atoms/MyAvatar/MyAvatar.tsx
+++ apps/casaub0n-page/src/components/atoms/MyAvatar/MyAvatar.tsx
@@ -1,4 +1,3 @@
-"use client";
 import type { ComponentPropsWithoutRef, FC } from "react";
 
 import clsx from "clsx";
@@ -10,13 +9,25 @@ type Props = ComponentPropsWithoutRef<"div">;
 
 /**
  * My Image Avatar by next/image
- * @description vercel can host image file.
- * @see https://github.com/vercel/next.js/blob/2c8606e59647714e5b74da46e4ff25bb63be37d9/examples/image-legacy-component/pages/layout-fill.tsx
+ * @description vercel can host local image file.
+ *
+ * The below refrence is next.js exampele
+ * @see https://github.com/vercel/next.js/tree/2c8606e59647714e5b74da46e4ff25bb63be37d9/examples/image-component
+ *
+ * Use margin because padding of this component cannot work
+ * @example <MyAvatar className={clsx(css({ marginTop: "2rem" }))} />
+ * @see https://sindresorhus.com/
+ *
+ * deployed story
+ * @see https://casaub0n.github.io/casaub0n/?path=/story/casaub0n-page-atoms-myavatar--default
+ *
+ * local story
+ * @see http://localhost:6006/?path=/story/casaub0n-page-atoms-myavatar--default
  */
 export const MyAvatar: FC<Props> = ({ className, ...props }) => {
   return (
     <div className={clsx(className, css({position: 'relative', width: '144px', height: '144px'}))} {...props}>
-      <Image alt='my picture' fill objectFit="cover" src="/images/me.jpg" style={{ borderRadius: "50%"}}/>
+      <Image className={css({ borderRadius: "50%" })} alt='my picture' fill objectFit="cover" src="/images/me.jpg" />
     </div>
   );
 };
