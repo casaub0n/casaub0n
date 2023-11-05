4923ca7391359979306a1ed9563950bcca77a49d
Casaubon <netzach.yoshida@gmail.com>
2023-11-06 00:46:58 +0900

    start project of learn nextjs (#757)
    * feat: 🎸 initial commit for learn nextjs-dashboard
    
    create project like below:
    npx create-next-app@latest nextjs-dashboard --use-npm --example "https://github.com/vercel/next-learn/tree/main/dashboard/starter-example"
    and then, remove node_modules and package-lock.json because that in pnpm's monorepo
    
    * feat: 🎸 add fonts
    
    Inter and Lusitana
    
    * fix: 🐛 eslint
    
    adjust lint for my monorepo
    
    * style: 💄 lint fix
    
    by next lint command
    
    * ci: 🎡 remove nextjs-dashboard build process
    
    nextjs-dashboard is my learning project for Next.js that is generated
    from WIP project from vercel repo, so it can't build proppery right now.

---
+++ apps/nextjs-dashboard/app/ui/fonts.ts
@@ -0,0 +1,8 @@
+import { Inter, Lusitana } from "next/font/google";
+
+export const inter = Inter({ subsets: ["latin"] });
+
+/**
+ * @see https://nextjs.org/learn/dashboard-app/optimizing-fonts-images#practice-adding-a-secondary-font
+ */
+export const lusitana = Lusitana({ weight: ["400", "700"], subsets: ["latin"] });
