e7ee86ba2126b06eec02a45c18e3fd9ee79b9b77
casaub0n <netzach.yoshida@gmail.com>
2023-11-16 21:41:23 +0900

    fix: 🐛 add requestheader

--- crates/rss_reader/src/main.rs
+++ crates/rss_reader/src/main.rs
@@ -1,12 +1,16 @@
-use windows::{
-    core::*,
-    Foundation::Uri,
-    Web::Syndication::SyndicationClient
-};
+// https://github.com/microsoft/windows-rs/blob/master/crates/samples/windows/rss/src/main.rs
 
-fn main() -> windows::core::Result<()> {
+use windows::{core::*, Foundation::Uri, Web::Syndication::SyndicationClient};
+
+fn main() -> Result<()> {
     let uri = Uri::CreateUri(h!("https://blogs.windows.com/feed"))?;
     let client = SyndicationClient::new()?;
+
+    client.SetRequestHeader(
+        h!("User-Agent"),
+        h!("Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.2; WOW64; Trident/6.0)"),
+    )?;
+
     let feed = client.RetrieveFeedAsync(&uri)?.get()?;
 
     for item in feed.Items()? {
