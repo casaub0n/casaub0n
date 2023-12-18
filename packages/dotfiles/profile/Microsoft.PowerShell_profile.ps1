bcb705b5e71177a3d53dede7227045ba2861aebf
Casaubon <netzach.yoshida@gmail.com>
2023-12-18 23:59:56 +0900

    refactor: 💡 compoze the function for fzf (#871)

--- packages/dotfiles/profile/Microsoft.PowerShell_profile.ps1
+++ packages/dotfiles/profile/Microsoft.PowerShell_profile.ps1
@@ -17,30 +17,6 @@ Set-PSReadLineKeyHandler -Key Tab -ScriptBlock { Invoke-FzfTabCompletion }
 # https://cli.github.com/manual/gh_completion
 Invoke-Expression -Command $(gh completion -s powershell | Out-String)
 
-# https://zenn.dev/mebiusbox/articles/b922c7e6ded49a
-function navi() {
-    $data = ConvertFrom-Yaml (Get-Content -raw $env:USERPROFILE\navi.yaml)
-    $command = $data.items.keys | ForEach-Object {
-        Write-Output ("{0}`t{1}`t{2}" -f $_, $data.items.($_).description, $data.items.($_).command)
-    }
-    | fzf --reverse --ansi --preview 'echo {} | sd "\"(\w+)\t+(.+)\t+(.+)\"" "\033[0;36m$2 \033[0;32m[$1]\n\033[1;93m> $3\033[0m"' --preview-window=up:40% --tabstop=50
-    if ($command -ne "") {
-        if ($command -match '(\w+)\t+(.+)\t+(.+)') {
-            if ($args.Length -gt 0 -and $args[0] -eq "-r") {
-                Write-Host ("> {0}" -f $Matches.3)
-                $Matches.3 | Invoke-Expression
-            }
-            else {
-                Write-Output $Matches.3
-                [Microsoft.PowerShell.PSConsoleReadLine]::AddToHistory($Matches.3)
-            }
-        }
-    }
-}
-function nav() {
-    navi("-r")
-}
-
 # @see https://learn.microsoft.com/ja-jp/powershell/module/psreadline/about/about_psreadline?view=powershell-7.4
 $parameters = @{
     key              = 'Alt-w'
