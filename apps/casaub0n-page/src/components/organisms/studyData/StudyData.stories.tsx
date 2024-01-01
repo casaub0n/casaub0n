9518a3270ca8165dd5b5d9534d59a2482eddc33a
Casaubon <netzach.yoshida@gmail.com>
2024-01-02 01:12:36 +0900

    show docs in storybook and fix eslint (#901)
    * fix: 🐛 eslint file type
    change csj to js
    
    * feat: 🎸 show docs in storybook

--- apps/casaub0n-page/src/components/organisms/studyData/StudyData.stories.tsx
+++ apps/casaub0n-page/src/components/organisms/studyData/StudyData.stories.tsx
@@ -5,10 +5,18 @@ import type { Meta, StoryObj } from "@storybook/react";
 const meta = {
   title: "organisms/StudyData",
   component: StudyDataComponent,
+  tags: ["autodocs"],
+  parameters: {
+    componentSubtitle: "Display study data graph"
+  }
 } satisfies Meta<typeof StudyDataComponent>;
 
 export default meta;
 
 type Story = StoryObj<typeof meta>;
 
-export const Default: Story = {};
+export const Default: Story = {
+  args: {
+    className: "study-data"
+  }
+};
