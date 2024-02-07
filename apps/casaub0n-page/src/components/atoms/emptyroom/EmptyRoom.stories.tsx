676e0037b048ffd11f6d8174b371ff2781eefcc6
casaub0n <netzach.yoshida@gmail.com>
2024-02-07 17:52:44 +0900

    refactor: 💡 remove unused components

--- apps/casaub0n-page/src/components/atoms/emptyroom/EmptyRoom.stories.tsx
+++ apps/casaub0n-page/src/components/atoms/emptyroom/EmptyRoom.stories.tsx
@@ -1,17 +1,17 @@
-import { Main } from "./Main";
+import { EmptyRoomImage } from "./EmptyRoom";
 
 import type { Meta, StoryObj } from "@storybook/react";
 
 const meta = {
-  title: "molecules/main",
-  component: Main,
+  title: "molecules/empty room",
+  component: EmptyRoomImage,
   tags: ["autodocs"],
-} satisfies Meta<typeof Main>;
+} satisfies Meta<typeof EmptyRoomImage>;
 
 export default meta;
 
 type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
-  name: "Default Main",
+  name: "Default empty room",
 };
--- apps/casaub0n-page/src/components/atoms/emptyroom/Emptyroom.test.tsx
+++ apps/casaub0n-page/src/components/atoms/emptyroom/Emptyroom.test.tsx
@@ -1,13 +1,13 @@
 import { composeStories } from "@storybook/react";
 import { render } from "@testing-library/react";
 
-import * as stories from "./ContentText.stories";
+import * as stories from "./EmptyRoom.stories";
 
 const { Default } = composeStories(stories);
 
-describe("src/component/atoms/ContentText.test.tsx", () => {
+describe("src/component/atoms/EmptyRoom.test.tsx", () => {
   test("Atom", () => {
     const { container } = render(<Default />);
     expect(container).toBeAtom();
-  });
+  })
 });
