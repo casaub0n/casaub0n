372a6114d053250c6df9230320f25c299c6ec7ef
Casaubon <netzach.yoshida@gmail.com>
2024-02-26 12:37:02 +0900

    feat: 🎸 option y (#1006)

--- packages/test-driven-development-by-example/src/pyAlgo/index.ts
+++ packages/test-driven-development-by-example/src/pyAlgo/index.ts
@@ -1,3 +1,10 @@
+// https://twitter.com/t_wada/status/904916106153828352
+// コードには How
+// テストコードには What
+// コミットログには Why
+// コードコメントには Why not
+// を書こうという話をした
+
 export const getFirst = <T>(myList: T[]) => {
   return myList[0];
 };
@@ -23,3 +30,9 @@ export const ConstractaWithOptionG = new RegExp("ABC", "g");
 export const LiteralWithOptionG = /ABC/g;
 
 export const regExpOptionI = "ABC".match(/ABC/i);
+export const regExpOptionM = "ABC\nDEF".match(/^D/m);
+
+/**
+ * y option is not enable in String Object
+ */
+export const regExpOptionY = /ABC/y;
