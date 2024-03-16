30144dd3a2dc60cb52a7eb324db12e3cba762680
casaub0n <netzach.yoshida@gmail.com>
2024-03-17 00:55:27 +0900

    style: 💄 algorism and regExp project move to tsbox

---
+++ packages/tsbox/src/algorism/algorism.test.ts
@@ -0,0 +1,19 @@
+import { test, expect } from "vitest";
+
+import { getFirst, getSum } from "./index";
+
+test("the first number 1 in 1, 2, 3", () => {
+  expect(getFirst([1, 2, 3])).toBe(1);
+});
+
+test("the first number 1 in 1, 2, 3, 4, 5, 6, 7, 8, 9, 10", () => {
+  expect(getFirst([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])).toBe(1);
+});
+
+test("1 + 2 + 3 = 6", () => {
+  expect(getSum([1, 2, 3])).toBe(6);
+});
+
+test("1 + 2 + 3 + 4 = 10", () => {
+  expect(getSum([1, 2, 3, 4])).toBe(10);
+});
