3cf932bc0a0940f40749fef4b46327fffc933a44
Casaubon <netzach.yoshida@gmail.com>
2024-06-19 14:23:12 +0900

    feat: 🎸 exhaustiveness (#1759)

--- packages/tsbox/src/algorism/algorism.test.ts
+++ packages/tsbox/src/algorism/algorism.test.ts
@@ -1,6 +1,6 @@
 import { test, expect } from "vitest";
 
-import { getFirst, getSum } from "./index";
+import { exhaustive, getFirst, getSum } from "./index";
 
 test("the first number 1 in 1, 2, 3", () => {
   expect(getFirst([1, 2, 3])).toBe(1);
@@ -16,4 +16,8 @@ test("1 + 2 + 3 = 6", () => {
 
 test("1 + 2 + 3 + 4 = 10", () => {
   expect(getSum([1, 2, 3, 4])).toBe(10);
+});
+
+test("exhaustiveness", () => {
+  expect(exhaustive("a")).toBe("A");
 });
