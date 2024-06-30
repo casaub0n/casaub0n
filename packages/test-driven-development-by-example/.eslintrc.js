7ea9ba9b9c4d78ee8ef0e12aa28f192b9694ae9c
casaub0n <netzach.yoshida@gmail.com>
2024-06-30 19:33:34 +0900

    fix: 🐛 eslint

--- packages/test-driven-development-by-example/.eslintrc.js
+++ packages/test-driven-development-by-example/.eslintrc.js
@@ -1,6 +1,6 @@
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
-  extends: ["@repo/eslint-config/index.js"],
+  extends: ["@casaub0n/eslint-config/index.js"],
   parser: "@typescript-eslint/parser",
   parserOptions: {
     project: true,
