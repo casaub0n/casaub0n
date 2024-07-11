5be17e67f5566f6f075a6af8a727d791ab9917ea
Casaubon <netzach.yoshida@gmail.com>
2024-07-12 01:28:32 +0900

    style: 💄 lint everything (#1899)

--- packages/spidering/src/utils/Result.ts
+++ packages/spidering/src/utils/Result.ts
@@ -2,7 +2,6 @@ export type Result<T, E> = Success<T, E> | Failure<T, E>;
 
 export class DoSomethingError extends Error {}
 export class Success<T, E> {
-  // eslint-disable-next-line no-unused-vars
   constructor(readonly value: T) {}
   type = "success" as const;
   isSuccess(): this is Success<T, E> {
@@ -14,7 +13,6 @@ export class Success<T, E> {
 }
 
 export class Failure<T, E> {
-  // eslint-disable-next-line no-unused-vars
   constructor(readonly value: E) {}
   type = "failure" as const;
   isSuccess(): this is Success<T, E> {
