922081ef5775d043858ab04d494c812c4a8f46c8
casaub0n <netzach.yoshida@gmail.com>
2024-07-19 15:02:06 +0900

    fix: 🐛 remove tsup

---
+++ packages/ush/build.ts
@@ -0,0 +1,19 @@
+/* eslint-disable no-console */
+
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { build } from "esbuild";
+
+const outDir = "dist";
+
+void (async () => {
+  await build({
+    entryPoints: ["src/index.ts"],
+    bundle: true,
+    minify: true,
+    platform: "node",
+    target: "node20",
+    outfile: `${outDir}/main.js`,
+  });
+})();
+
+console.log("Build completed!");
