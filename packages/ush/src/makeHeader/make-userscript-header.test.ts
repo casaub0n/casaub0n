ec30ee66365e93491683b23e0244ddb9c5f5838e
casaub0n <netzach.yoshida@gmail.com>
2024-08-02 17:53:44 +0900

    test: 💍 more test

--- packages/ush/src/makeHeader/make-userscript-header.test.ts
+++ packages/ush/src/makeHeader/make-userscript-header.test.ts
@@ -25,3 +25,27 @@ test("the version in output string is package.json's version if sameversion true
 // ==/UserScript==\n\n`;
   expect(makeUserscriptHeader(inputObj, "2.0.0")).toStrictEqual(outputStr);
 });
+
+test("the version in output string is userscript.json's version if sameversion false", () => {
+  const inputObj: UserScript = {
+    name: "test obj",
+    namespace: "NONE",
+    version: "1.0.0",
+    description: "testing is good",
+    author: "casaub0n",
+    match: "true",
+    grant: "NONE",
+    sameversion: false,
+  };
+
+  const outputStr = `// ==UserScript==\n
+// @name         test obj\n
+// @namespace    NONE\n
+// @version      1.0.0\n
+// @description  testing is good\n
+// @author       casaub0n\n
+// @match        true\n
+// @grant        NONE\n
+// ==/UserScript==\n\n`;
+  expect(makeUserscriptHeader(inputObj, "2.0.0")).toStrictEqual(outputStr);
+});
