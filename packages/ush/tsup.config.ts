bafdcbf1bce85773c808e87ecc10a8a4f1e5c782
Casaubon <netzach.yoshida@gmail.com>
2024-08-09 14:22:22 +0900

    feat: 🎸 change bundler (#2047)
    esbuild to tsup

---
+++ packages/ush/tsup.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from "tsup";
+
+/**
+ * bundle all dependency
+ * @see https://github.com/egoist/tsup/issues/619#issuecomment-2248676853
+ */
+export default defineConfig({
+  format: ["esm"],
+  entry: ["./src/index.ts"],
+  dts: false,
+  shims: true,
+  skipNodeModulesBundle: true,
+  clean: true,
+  target: "node20",
+  platform: "node",
+  minify: true,
+  bundle: true,
+  // https://github.com/egoist/tsup/issues/619
+  noExternal: [/(?:.*)/],
+  splitting: false,
+});
