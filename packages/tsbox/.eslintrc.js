f993bcd42317cf6e69802eb76810015ce0a0d129
Casaubon <netzach.yoshida@gmail.com>
2024-10-05 13:25:10 +0900

    fix: 🐛 eslint command
    install eslint.config.mjs and correct lint command in package.json

--- packages/tsbox/.eslintrc.js
+++
@@ -1,8 +0,0 @@
-/** @type {import("eslint").Linter.Config} */
-module.exports = {
-  extends: ["@casaub0n/eslint-config/index.js"],
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    project: true,
-  },
-};
