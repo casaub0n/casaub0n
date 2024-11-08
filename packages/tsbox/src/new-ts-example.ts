4c5abe4a5510eb5364c8bfb7bb40fc2a51035ed4
Casaubon <netzach.yoshida@gmail.com>
2024-11-09 02:47:18 +0900

    feat: 🎸 add Recursive Type in TypeScript example

--- packages/tsbox/src/new-ts-example.ts
+++ packages/tsbox/src/new-ts-example.ts
@@ -26,4 +26,56 @@ await requestExternalApi({
   },
 });
 
+/**
+ * `as const` and `satisfies`
+ * @see https://zenn.dev/ficilcom/articles/940ecce71e45a6#as-const%E3%81%A8satisfies
+ */
+const localizeDataJP = {
+  novel: {
+    chapter: "チャプター",
+    episode: "話",
+  },
+} as const satisfies {
+  novel: {
+    chapter: string;
+    episode: string;
+  };
+};
+
+// const chapter = localizeData_JP.novel.chapter;
+
+/**
+ * Recursive Type
+ * @see https://zenn.dev/ficilcom/articles/940ecce71e45a6#%E5%86%8D%E5%B8%B0%E7%9A%84
+ */
+type RecursiveObj = { [key: string]: string | RecursiveObj };
+
+type UnionWithDot<
+  Prefix extends string,
+  Key,
+> = `${Prefix}${Prefix extends "" ? "" : "."}${Key extends string ? Key : ""}`;
+
+type DotKeys<Obj extends RecursiveObj, Prefix extends string = ""> = {
+  [Key in keyof Obj]: Obj[Key] extends object
+    ? DotKeys<Obj[Key], UnionWithDot<Prefix, Key>>
+    : UnionWithDot<Prefix, Key>;
+}[keyof Obj];
+
+const localize = (key: ExtractedDotKeys) => {
+  const splittedKey = key.split(".");
+
+  const stringOrObj = splittedKey.reduce<RecursiveObj | string>((obj, key) => {
+    if (typeof obj === "string") {
+      return obj;
+    }
+
+    return obj[key];
+  }, localizeDataJP);
+};
+
+// novel.chap is type error
+localize("novel.chapter");
+
+export type ExtractedDotKeys = DotKeys<typeof localizeDataJP>;
+
 export {};
