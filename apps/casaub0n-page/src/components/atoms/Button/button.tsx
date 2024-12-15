28da6126c1fb995795f6254f6cb259b134258810
Casaubon <netzach.yoshida@gmail.com>
2024-12-15 15:09:05 +0900

    fix: 🐛 button
    use FC for React type

--- apps/casaub0n-page/src/components/atoms/Button/button.tsx
+++ apps/casaub0n-page/src/components/atoms/Button/button.tsx
@@ -1,4 +1,4 @@
-import type { ButtonHTMLAttributes } from "react";
+import type { ButtonHTMLAttributes, FC } from "react";
 import { clsx } from "clsx";
 import { button , size, visual } from "./button.css";
 
@@ -10,10 +10,12 @@ interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   visualVariant?: keyof typeof visual
 }
 
+
+// const Button({sizeVariant = "medium", visualVariant="primary", children, className="simple-button", ...props}: ButtonProps): JSX.Element {
 /**
  * @see https://zenn.dev/u_10/articles/8c3cda00a701e9
  */
-function Button({sizeVariant = "medium", visualVariant="primary", children, className="simple-button", ...props}: ButtonProps): JSX.Element {
+const Button: FC<ButtonProps> = ({sizeVariant = "medium", visualVariant="primary", children, className="simple-button", ...props}) => {
   return (
     <button className={clsx(button, size[sizeVariant], visual[visualVariant], className)} {...props}>
       {children}
