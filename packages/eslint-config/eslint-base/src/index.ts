fc34c7aaf58c9266172cb4e07aa2cb0e2e493dee
Casaubon <netzach.yoshida@gmail.com>
2025-02-18 15:07:18 +0900

    fix: 🐛 use tseslint config and remove next.js lint (#3358)

--- packages/eslint-config/eslint-base/src/index.ts
+++ packages/eslint-config/eslint-base/src/index.ts
@@ -1,20 +1,23 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+// import { dirname } from "path";
+// import { fileURLToPath } from "url";
+// import { FlatCompat } from "@eslint/eslintrc";
 import tseslint from "typescript-eslint";
 import unuserdPlugin from "eslint-plugin-unused-imports";
 import type TSESLint from "@typescript-eslint/utils";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = dirname(__filename);
 
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
+// const compat = new FlatCompat({
+//   baseDirectory: __dirname,
+// });
 
+/**
+ * Gemini: https://g.co/gemini/share/5086f0d94b4e
+ */
 const config = tseslint.config({
   extends: [
-    ...compat.extends("next/core-web-vitals", "next/typescript"),
+    // ...compat.extends("next/core-web-vitals", "next/typescript"),
     ...tseslint.configs.strict,
     ...tseslint.configs.stylistic,
   ],
@@ -25,7 +28,4 @@ const config = tseslint.config({
   },
 }) as TSESLint.TSESLint.FlatConfig.Config;
 
-/**
- * @see https://g.co/gemini/share/5086f0d94b4e
- */
 export default config;
--- packages/test-driven-development-by-example/eslint.config.mjs
+++ packages/test-driven-development-by-example/eslint.config.mjs
@@ -1,3 +1,12 @@
-import base from "../config-eslint/base.mjs";
+import base from "../eslint-config/eslint-base/dist/index.mjs";
 
-export default [...base, { rules: { "no-console": "off", "import/no-unresolved": "off" } }];
+export default [
+  ...base,
+  {
+    rules: {
+      "no-console": "off",
+      "import/no-unresolved": "off",
+      "@typescript-eslint/consistent-type-definitions": "off",
+    },
+  },
+];
