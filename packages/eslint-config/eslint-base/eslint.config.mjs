87cdc844c021f79810576e1055dcf6969a42212f
Casaubon <netzach.yoshida@gmail.com>
2025-02-22 13:02:19 +0900

    fix eslint config (#3401)
    * fix: 🐛 eslint config
    add my eslint in eslint config

---
+++ packages/eslint-config/eslint-base/eslint.config.mjs
@@ -0,0 +1,12 @@
+import base from "./dist/index.mjs";
+
+export default [
+  ...base,
+  {
+    rules: {
+      "no-console": "off",
+      "import/no-unresolved": "off",
+      "@typescript-eslint/consistent-type-definitions": "off",
+    },
+  },
+];
