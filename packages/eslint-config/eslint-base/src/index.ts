f0bec4c4c5d8e47c46bf71b4f01b9ba62e4173d8
Casaubon <netzach.yoshida@gmail.com>
2025-03-02 17:19:54 +0900

    feat: 🎸 eslint more strict

--- packages/eslint-config/eslint-base/src/index.ts
+++ packages/eslint-config/eslint-base/src/index.ts
@@ -7,6 +7,7 @@ import type TSESLint from "@typescript-eslint/utils";
 import cspellPlugin from "@cspell/eslint-plugin";
 import eslintPluginUnicorn from "eslint-plugin-unicorn";
 import turboPlugin from "eslint-plugin-turbo";
+import pluginConfigPrettier from "eslint-config-prettier";
 
 /**
  * by eslint rule
@@ -24,35 +25,70 @@ const compat = new FlatCompat({
 /**
  * Gemini: https://g.co/gemini/share/5086f0d94b4e
  */
-const config = tseslint.config({
-  extends: [
-    // ...compat.extends("next/core-web-vitals", "next/typescript"),
-    ...compat.extends(),
-    ...tseslint.configs.strict,
-    ...tseslint.configs.stylistic,
-  ],
-  plugins: {
-    "unused-imports": unuserdPlugin,
-    "@cspell": cspellPlugin,
-    unicorn: eslintPluginUnicorn,
-    turbo: turboPlugin,
-  },
-  rules: {
-    // ESlint core
-    curly: "error",
-    "@typescript-eslint/no-unused-vars": "error",
-    "unused-imports/no-unused-imports": "error",
-    // https://github.com/streetsidesoftware/cspell/tree/main/packages/cspell-eslint-plugin#configuration-new-eslintconfigjs
-    "@cspell/spellchecker": ["warn", {}],
-    // https://zenn.dev/yskn_sid25/articles/c309f804fde5a5#%E3%82%BB%E3%83%83%E3%83%88%E3%82%A2%E3%83%83%E3%83%97
-    ...eslintPluginUnicorn.configs.recommended.rules,
-    "turbo/no-undeclared-env-vars": [
-      "error",
-      {
-        allowList: ["^ENV_[A-Z]+$"],
-      },
+const config = tseslint.config([
+  {
+    extends: [
+      // ...compat.extends("next/core-web-vitals", "next/typescript"),
+      ...compat.extends(),
+      ...tseslint.configs.strict,
+      ...tseslint.configs.stylistic,
     ],
+    plugins: {
+      "unused-imports": unuserdPlugin,
+      "@cspell": cspellPlugin,
+      unicorn: eslintPluginUnicorn,
+      turbo: turboPlugin,
+    },
+    rules: {
+      /**
+       * [eslint-config/base/index.js at master · herp-inc/eslint-config](https://github.com/herp-inc/eslint-config/blob/master/base/index.js)
+       */
+      // ESlint core
+      curly: "error",
+      "default-case-last": "error",
+      eqeqeq: "error",
+      "no-console": "error",
+      "no-else-return": ["error", { allowElseIf: false }],
+      "no-lonely-if": "error",
+      "no-multi-assign": "error",
+      "no-negated-condition": "error",
+      "no-new": "error",
+      "no-new-object": "error",
+      "no-new-wrappers": "error",
+      "no-param-reassign": "error",
+      "no-return-assign": "error",
+      "no-self-compare": "error",
+      "no-sequences": ["error", { allowInParentheses: false }],
+      "no-unmodified-loop-condition": "error",
+      "no-unneeded-ternary": "error",
+      "no-useless-backreference": "error",
+      "no-useless-concat": "error",
+      "no-useless-rename": "error",
+      "no-useless-return": "error",
+      "object-shorthand": "error",
+      "one-var": ["error", "never"],
+      "prefer-object-spread": "error",
+      "@typescript-eslint/no-unused-vars": "error",
+      /**
+       * [最低限の flat config（まずは no-unused-imports を動かす）](https://zenn.dev/seventhseven07/articles/06a02c4048decf)
+       */
+      "unused-imports/no-unused-imports": "error",
+      // https://github.com/streetsidesoftware/cspell/tree/main/packages/cspell-eslint-plugin#configuration-new-eslintconfigjs
+      "@cspell/spellchecker": ["warn", {}],
+      // https://zenn.dev/yskn_sid25/articles/c309f804fde5a5#%E3%82%BB%E3%83%83%E3%83%88%E3%82%A2%E3%83%83%E3%83%97
+      ...eslintPluginUnicorn.configs.recommended.rules,
+      "turbo/no-undeclared-env-vars": [
+        "error",
+        {
+          allowList: ["^ENV_[A-Z]+$"],
+        },
+      ],
+    },
   },
-}) satisfies TSESLint.TSESLint.FlatConfig.ConfigArray;
+  /**
+   * https://zenn.dev/kazukix/articles/eslint-config-2024-09#eslint-config-prettier
+   */
+  pluginConfigPrettier,
+]) satisfies TSESLint.TSESLint.FlatConfig.ConfigArray;
 
 export default config;
