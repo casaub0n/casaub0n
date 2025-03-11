545c4041a405f32a87dd0da68c9fee81bf56fe05
Casaubon <netzach.yoshida@gmail.com>
2025-03-11 11:47:08 +0900

    add comment

--- packages/eslint-config/eslint-base/src/index.ts
+++ packages/eslint-config/eslint-base/src/index.ts
@@ -43,6 +43,19 @@ const ignoreConfig = {
 
 /**
  * Gemini: https://g.co/gemini/share/5086f0d94b4e
+ * @example ```js
+ * import base from "../eslint-config/eslint-base/dist/index.mjs";
+ *
+ * export default [
+ *   ...base,
+ *   {
+ *     rules: {
+ *       "no-console": "off",
+ *       "import/no-unresolved": "off",
+ *       "@typescript-eslint/consistent-type-definitions": "off",
+ *     }
+ * ]
+ * ```
  */
 const config = tseslint.config([
   ignoreConfig,
