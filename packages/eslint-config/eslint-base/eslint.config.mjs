f3f26876a339335bd4d84b8774e935b698c77641
Casaubon <netzach.yoshida@gmail.com>
2025-03-13 01:30:43 +0900

    fix: 🐛 eslint

--- packages/eslint-config/eslint-base/eslint.config.mjs
+++ packages/eslint-config/eslint-base/eslint.config.mjs
@@ -1,8 +1,29 @@
+// import path from "node:path";
+// import { fileURLToPath } from "node:url";
 import base from "./dist/index.mjs";
 
+// const { dirname } = path;
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = dirname(__filename);
+
 export default [
+  // {
+  //   ignores: ["dist/**"],
+  // },
   ...base,
   {
+    //   languageOptions: {
+    //     parserOptions: {
+    //       projectService: {
+    //         allowDefaultProject: ["./eslint.config.mjs", "./dist/index.mjs", "*.mjs"],
+    //         defaultProject: true,
+    //       },
+    //       project: true,
+    //       // projectService: true,
+    //       tsconfigRootDir: __dirname,
+    //       // projectFolderIgnoreList: ["**dist**"],
+    //     },
+    //   },
     rules: {
       "no-console": "off",
       "import/no-unresolved": "off",
