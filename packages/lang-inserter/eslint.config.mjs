d7710f9c94824a3d24a54a209f94a7bd99c121ff
Casaubon <netzach.yoshida@gmail.com>
2025-03-26 14:33:00 +0900

    refactor: 💡 lang-inserter (#3643)

--- packages/lang-inserter/eslint.config.mjs
+++ packages/lang-inserter/eslint.config.mjs
@@ -1,15 +1,15 @@
 import base from "../eslint-config/eslint-base/dist/index.mjs";
 
 const baseEslint = [
-  {
-    settings: {
-      "import/resolver": {
-        node: {
-          extensions: [".js", ".jsx", ".ts", ".tsx"],
-        },
-      },
-    },
-  },
+  // {
+  //   settings: {
+  //     "import/resolver": {
+  //       node: {
+  //         extensions: [".js", ".jsx", ".ts", ".tsx"],
+  //       },
+  //     },
+  //   },
+  // },
   ...base,
 ];
 
