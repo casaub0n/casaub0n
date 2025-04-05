40ccd5b5b232bc0711663eb2cc03db31a9e1fce7
Casaubon <netzach.yoshida@gmail.com>
2025-04-06 00:56:40 +0900

    feat: 🎸 rolldown

--- packages/header-inserter/eslint.config.mjs
+++ packages/header-inserter/eslint.config.mjs
@@ -1,10 +1,3 @@
 import base from "../eslint-config/eslint-base/dist/index.mjs";
 
-export default [
-  ...base,
-  {
-    rules: {
-      "import/no-unresolved": "off",
-    },
-  },
-];
+export default [...base];
