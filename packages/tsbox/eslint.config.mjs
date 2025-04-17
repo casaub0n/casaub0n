15dd6eb33a92c8e689c1b57217c48847d3783f8b
Casaubon <netzach.yoshida@gmail.com>
2025-04-17 20:10:50 +0900

    refactor: 💡 remove unused config (#3827)

--- packages/tsbox/eslint.config.mjs
+++ packages/tsbox/eslint.config.mjs
@@ -1,14 +1,3 @@
 import base from "../eslint-config/eslint-base/dist/index.mjs";
 
-export default [
-  ...base,
-  {
-    rules: {
-      "import/no-unresolved": "off",
-      "@typescript-eslint/strict-boolean-expressions": "off",
-      "no-console": "off",
-      "@typescript-eslint/explicit-function-return-type": "off",
-      "@typescript-eslint/no-unsafe-return": "off",
-    },
-  },
-];
+export default [...base];
