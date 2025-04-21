7ee474fdc2b30f32d407ffe210f598b978ee3d7d
Casaubon <netzach.yoshida@gmail.com>
2025-04-22 02:10:23 +0900

    perf: ⚡️ more better bundle
    for rolldown

--- packages/header-inserter/rolldown.config.ts
+++ packages/header-inserter/rolldown.config.ts
@@ -1,9 +1,28 @@
+/* eslint-disable @cspell/spellchecker -- @todo add dictionary */
 import { defineConfig } from "rolldown";
 
 export default defineConfig({
   input: "src/index.ts",
+
+  /**
+   * This package for development
+   */
+  platform: "node",
+
+  /**
+   * @todo Compare bundle size
+   */
+  treeshake: true,
+
+  /**
+   * Minification is powered by oxc-minifier, which is currently still work-in-progress.
+   * Current Rolldown version is 1.0.0-beta.7
+   */
   output: {
+    /**
+     * This option is required by `platform` because of multiple chunks to run Node.js native dependencies.
+     */
+    inlineDynamicImports: true,
     file: "dist/index.js",
-    minify: true,
   },
 });
