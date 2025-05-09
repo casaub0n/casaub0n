282dee60dd04194d35de210fb992704ced150c3a
Casaubon <netzach.yoshida@gmail.com>
2025-05-09 20:09:17 +0900

    refactor: 💡 utils (#4011)

---
+++ packages/eslint-config/utils/tsup.config.ts
@@ -0,0 +1,26 @@
+import { defineConfig } from "tsup";
+
+/**
+ * bundle all dependency
+ * @see https://github.com/egoist/tsup/issues/619#issuecomment-2248676853
+ */
+export default defineConfig({
+  format: ["esm"],
+  entry: {
+    "ignore-config": "./src/ignore-config.ts",
+  },
+  dts: true,
+  shims: true,
+  skipNodeModulesBundle: true,
+  clean: true,
+  target: "esnext",
+  platform: "node",
+  minify: true,
+  // bundle: true,
+  // https://github.com/egoist/tsup/issues/619
+  // noExternal: [/(?:.*)/],
+  splitting: false,
+  // banner: {
+  //   js: 'import { createRequire } from "module";const require = createRequire(import.meta.url);',
+  // },
+});
