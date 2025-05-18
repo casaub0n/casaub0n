0380b9307de213837c6a00e8de4396af250c865a
Casaubon <netzach.yoshida@gmail.com>
2025-05-18 12:38:38 +0900

    fix: 🐛 config

--- packages/eslint-config/eslint-base/src/index.ts
+++ packages/eslint-config/eslint-base/src/index.ts
@@ -113,7 +113,9 @@ const config = ({
 
     json.configs.recommended,
 
-    ...markdown.configs.recommended,
+    {
+      ...markdown.configs.recommended,
+    },
 
     /**
      * https://zenn.dev/kazukix/articles/eslint-config-2024-09#eslint-config-prettier
