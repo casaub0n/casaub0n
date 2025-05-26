8d0abe53f84042b901afd659d26e7dd909b42522
Casaubon <netzach.yoshida@gmail.com>
2025-05-26 21:37:31 +0900

    refactor eslint config (#4143)
    * refactor: 💡 remove tsup, use tsdown

---
+++ packages/eslint-config/utils/tsdown.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from "tsdown";
+
+export default defineConfig({
+  entry: {
+    "ignore-config": "./src/ignore-config.ts",
+    "eslint-core-rules": "./src/eslint-core-rules.ts",
+    "typescript-rules": "./src/typescript-rules.ts",
+  },
+  shims: true,
+  dts: true,
+  clean: true,
+  format: ["esm"],
+  target: "esnext",
+  platform: "node",
+  minify: true,
+  skipNodeModulesBundle: true,
+});
