7164ceed297758c5e3e04a349afe59477b6cf435
Casaubon <netzach.yoshida@gmail.com>
2025-06-14 13:00:30 +0900

    feat: 🎸 new nextjs

---
+++ apps/next-casaub0n/eslint.config.mjs
@@ -0,0 +1,10 @@
+import { defineConfig } from "eslint/config";
+import base from "@casaub0n/eslint-config-next";
+
+export default defineConfig([
+  ...base({
+    tsconfigRootDir: import.meta.dirname,
+    tsconfigFileName: import.meta.dirname + "/tsconfig.json",
+    rootDirectory: import.meta.dirname,
+  }),
+]);
