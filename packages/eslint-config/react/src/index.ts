e55bf6c5b084768bbca5c1336db1066e5b42a960
Casaubon <netzach.yoshida@gmail.com>
2025-06-15 23:36:08 +0900

    feat: 🎸 eslint for ui (#4345)

--- packages/eslint-config/react/src/index.ts
+++ packages/eslint-config/react/src/index.ts
@@ -1,9 +1,64 @@
-import type TSESLint from "@typescript-eslint/utils";
-import tseslint from "typescript-eslint";
+/**
+ * https://github.com/shadcn-ui/ui/blob/main/templates/monorepo-next/packages/eslint-config/react-internal.js
+ * https://github.com/jsx-eslint/eslint-plugin-react/issues/3878
+ */
+
 import js from "@eslint/js";
 import eslintConfigPrettier from "eslint-config-prettier";
+// @ts-expect-error -- no type config
+import onlyWarn from "eslint-plugin-only-warn";
+import turboPlugin from "eslint-plugin-turbo";
+import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
 import pluginReactHooks from "eslint-plugin-react-hooks";
 import globals from "globals";
+import type TSESLint from "@typescript-eslint/utils";
 
-const config = (): TSESLint.TSESLint.FlatConfig.ConfigArray => tseslint.config([]);
+const config = tseslint.config([
+  js.configs.recommended,
+  eslintConfigPrettier,
+  ...tseslint.configs.recommended,
+  {
+    plugins: {
+      turbo: turboPlugin,
+    },
+    rules: {
+      "turbo/no-undeclared-env-vars": "warn",
+    },
+  },
+  {
+    plugins: {
+      onlyWarn,
+    },
+  },
+  {
+    ignores: ["dist/**"],
+  },
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  pluginReact.configs.flat.recommended as any,
+  {
+    languageOptions: {
+      // @ts-expect-error https://github.com/jsx-eslint/eslint-plugin-react/issues/3878
+      ...pluginReact.configs.flat.recommended.languageOptions,
+      globals: {
+        ...globals.serviceworker,
+        ...globals.browser,
+      },
+    },
+  },
+  {
+    plugins: {
+      "react-hooks": pluginReactHooks,
+    },
+    settings: { react: { version: "detect" } },
+    rules: {
+      ...pluginReactHooks.configs.recommended.rules,
+      // React scope no longer necessary with new JSX transform.
+      "react/react-in-jsx-scope": "off",
+      "react/prop-types": "off",
+    },
+  },
+]) satisfies TSESLint.TSESLint.FlatConfig.ConfigArray;
+
+export default config;
