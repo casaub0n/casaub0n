dffacca936062646c671248887b91806ded96355
Casaubon <netzach.yoshida@gmail.com>
2025-07-19 13:54:28 +0900

    refactor: 💡 storybook (#4618)

--- apps/casaub0n-page/src/components/atoms/TweetSkelton/TweetSkelton.stories.tsx
+++ apps/casaub0n-page/src/components/atoms/TweetSkelton/TweetSkelton.stories.tsx
@@ -1,6 +1,6 @@
 import { TweetSkeleton } from "react-tweet";
 
-import type { Meta, StoryObj } from "@storybook/react";
+import type { Meta, StoryObj } from "@storybook/react-webpack5";
 
 const meta = {
   title: "atoms/Tweet Skelton",
--- apps/casaub0n-page/src/components/molecules/embedYoutube/EmbedYoutube.stories.tsx
+++ apps/casaub0n-page/src/components/molecules/embedYoutube/EmbedYoutube.stories.tsx
@@ -1,6 +1,6 @@
 import { EmbedYoutube } from "./EmbedYoutube";
 
-import type { Meta, StoryObj } from "@storybook/react";
+import type { Meta, StoryObj } from "@storybook/react-webpack5";
 
 const meta = {
   title: "molecules/EmbedYoutube",
