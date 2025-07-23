fc1b684a4165cfa040c9be9301a56093ddf7fb64
Casaubon <netzach.yoshida@gmail.com>
2025-07-23 13:14:22 +0900

    fix: 🐛 all config is set ignore mjs and js (#4643)
    remove ignore each config and install mjs and js config into original
    config

--- packages/eslint-config/next/eslint.config.mjs
+++ packages/eslint-config/next/eslint.config.mjs
@@ -2,10 +2,6 @@ import { defineConfig } from "eslint/config";
 import base from "@casaub0n/eslint-base";
 
 export default defineConfig([
-  // VSCode ESLint extension can't find `tsconfigRootDir`
-  {
-    ignores: ["**/*.mjs"],
-  },
   ...base({
     tsConfigurationRootDirectory: import.meta.dirname,
     tsconfigFileName: "./tsconfig.json",
--- packages/spidering/eslint.config.mjs
+++ packages/spidering/eslint.config.mjs
@@ -2,9 +2,6 @@ import { defineConfig } from "eslint/config";
 import base from "@casaub0n/eslint-base";
 
 export default defineConfig([
-  {
-    ignores: ["**/*.mjs"],
-  },
   ...base({
     tsConfigurationRootDirectory: import.meta.dirname,
     tsconfigFileName: "./tsconfig.json",
