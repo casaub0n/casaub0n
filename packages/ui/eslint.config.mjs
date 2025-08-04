75b46aed8493bdc11295cf5e581d26bad68e6791
Casaubon <netzach.yoshida@gmail.com>
2025-08-05 00:19:28 +0900

    fix: 🐛 lint
    almost disabled rules, just add type interface at import part

--- packages/ui/eslint.config.mjs
+++ packages/ui/eslint.config.mjs
@@ -6,4 +6,26 @@ export default defineConfig([
     tsConfigurationRootDirectory: import.meta.dirname,
     tsconfigFileName: "./tsconfig.json",
   }),
+  {
+    rules: {
+      "@typescript-eslint/explicit-function-return-type": "off",
+      /**
+       * [eslint-plugin-unicorn/docs/rules/prevent-abbreviations.md at main · sindresorhus/eslint-plugin-unicorn](https://github.com/sindresorhus/eslint-plugin-unicorn/blob/main/docs/rules/prevent-abbreviations.md#options)
+       */
+      "unicorn/prevent-abbreviations": [
+        "error",
+        {
+          replacements: {
+            props: {
+              properties: false,
+            },
+          },
+        },
+      ],
+      /**
+       * [eslint-plugin-unicorn/docs/rules/no-keyword-prefix.md at v60.0.0 · sindresorhus/eslint-plugin-unicorn](https://github.com/sindresorhus/eslint-plugin-unicorn/blob/v60.0.0/docs/rules/no-keyword-prefix.md#disallowedprefixes)
+       */
+      "unicorn/no-keyword-prefix": ["error", { disallowedPrefixes: ["className"] }],
+    },
+  },
 ]);
