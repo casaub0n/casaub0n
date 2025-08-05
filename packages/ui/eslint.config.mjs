bea0afd566c3ea08fc9609d408d218ee2f3cbe0b
Casaubon <netzach.yoshida@gmail.com>
2025-08-05 22:04:46 +0900

    fix: 🐛 lint
    adjust current project

--- packages/ui/eslint.config.mjs
+++ packages/ui/eslint.config.mjs
@@ -19,6 +19,9 @@ export default defineConfig([
             props: {
               properties: false,
             },
+            utils: {
+              utilities: false,
+            },
           },
         },
       ],
