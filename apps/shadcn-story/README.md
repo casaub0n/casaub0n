e99d711b82d4f93643da0a67afacc884316d0801
Casaubon <netzach.yoshida@gmail.com>
2025-08-06 01:17:33 +0900

    fix: 🐛 storybook (#4752)

--- apps/shadcn-story/README.md
+++ apps/shadcn-story/README.md
@@ -7,3 +7,22 @@ This project also partially contains code derived or copied from the following p
 - [ui/templates/monorepo-next at main · shadcn-ui/ui](https://github.com/shadcn-ui/ui/tree/main/templates/monorepo-next)
 - [turborepo/examples/design-system at main · vercel/turborepo](https://github.com/vercel/turborepo/tree/main/examples/design-system)
 - [Installing Tailwind CSS with Vite - Tailwind CSS](https://tailwindcss.com/docs/installation/using-vite)
+- [Astro + Tailwind CSS + shadcn/ui + Storybook を組み合わせたコンポーネント開発の環境設定｜板鼻祐治](https://note.com/itahana_depart/n/n5b946ddc2c2e)
+
+## WIP
+
+**error**
+
+```console
+storybook v9.1.1
+
+You are currently using Storybook 9.1.1 but you have packages which are incompatible with it:
+
+- @storybook/addon-essentials@8.6.14 which depends on 8.6.14
+ Repo: https://github.com/storybookjs/storybook/tree/next/code/addons/essentials
+
+Please consider updating your packages or contacting the maintainers for compatibility details.
+
+For more on Storybook 9 compatibility, see the linked GitHub issue:
+https://github.com/storybookjs/storybook/issues/30944
+```
--- apps/shadcn-story/src/index.ts
+++ apps/shadcn-story/src/index.ts
@@ -1,17 +1,14 @@
 import path from "node:path";
+import type { StorybookConfig } from "@storybook/react-vite";
+// eslint-disable-next-line import-x/no-unresolved
+import tailwindcss from "@tailwindcss/postcss";
+import autoprefixer from "autoprefixer";
 
-function getAbsolutePath(value: string): string {
-  return path.dirname(import.meta.resolve(path.join(value, "package.json")));
-}
-
-const config = {
-  stories: ["../**/*.mdx", "../**/*.stories.@(js|jsx|mjs|ts|tsx)"],
-  addons: [
-    getAbsolutePath("@storybook/addon-links"),
-    getAbsolutePath("@storybook/addon-essentials"),
-  ],
+const config: StorybookConfig = {
+  stories: ["../../../packages/ui/src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
   framework: {
-    name: getAbsolutePath("@storybook/react-vite"),
+    name: "@storybook/react-vite",
     options: {},
   },
 
@@ -19,24 +16,19 @@ const config = {
 
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   async viteFinal(config: any) {
-    // customize the Vite config here
-    return {
-      ...config,
-      define: { "process.env": {} },
+    return Object.assign(config, {
       resolve: {
-        alias: [
-          {
-            find: "ui",
-            replacement: import.meta.resolve(import.meta.dirname, "../../../packages/ui/"),
-          },
-        ],
+        alias: {
+          // eslint-disable-next-line unicorn/prefer-module
+          "@": path.resolve(__dirname, "../../../packages/ui/src"),
+        },
       },
-    };
-  },
-
-  docs: {
-    // eslint-disable-next-line @cspell/spellchecker
-    autodocs: true,
+      css: {
+        postcss: {
+          plugins: [tailwindcss(), autoprefixer()], // PostCSS 設定を適用
+        },
+      },
+    });
   },
 };
 
