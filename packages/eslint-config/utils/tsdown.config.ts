b5aeedf47bc7305bd0ab02124210f56be3231861
Casaubon <netzach.yoshida@gmail.com>
2025-08-19 12:10:30 +0900

    refactor: 💡 add index.js

--- packages/eslint-config/utils/tsdown.config.ts
+++ packages/eslint-config/utils/tsdown.config.ts
@@ -5,6 +5,7 @@ export default defineConfig({
     "ignore-config": "./src/ignore-config.ts",
     "eslint-core-rules": "./src/eslint-core-rules.ts",
     "typescript-rules": "./src/typescript-rules.ts",
+    index: "./src/index.ts",
   },
   shims: true,
   dts: true,
