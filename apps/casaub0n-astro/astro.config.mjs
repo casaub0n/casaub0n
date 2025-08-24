1790c25ad0de95002f1ab93c658972cb7c7dd61e
Casaubon <netzach.yoshida@gmail.com>
2025-08-24 23:42:14 +0900

    feature solid.js and GSAP (#4909)
    * feat: 🎸 solid.js and GSAP

--- apps/casaub0n-astro/astro.config.mjs
+++ apps/casaub0n-astro/astro.config.mjs
@@ -1,7 +1,9 @@
-import react from "@astrojs/react";
+// @ts-check
 import { defineConfig } from "astro/config";
+
+import solidJs from "@astrojs/solid-js";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
+  integrations: [solidJs()]
 });
