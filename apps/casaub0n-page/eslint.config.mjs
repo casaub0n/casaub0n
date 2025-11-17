221db96b2076bf1b121b4844f1cacda11138da54
Casaubon <netzach.yoshida@gmail.com>
2025-11-17 14:36:30 +0900

    fix: 🐛 eslint config (#5487)

--- apps/casaub0n-page/eslint.config.mjs
+++ apps/casaub0n-page/eslint.config.mjs
@@ -2,16 +2,22 @@ import { defineConfig } from "eslint/config";
 import base from "@casaub0n/eslint-config-next";
 
 export default defineConfig([
-  // VSCode ESLint extension can't find `tsconfigRootDir`
   {
     ignores: ["**/eslint.config.mjs"],
   },
   ...base({
     tsConfigurationRootDirectory: import.meta.dirname,
-    tsconfigFileName: "./tsconfig.json",
     rootDirectory: import.meta.dirname,
   }),
   {
+    ignores: [
+      /**
+       * Next.js
+       */
+      "**/.next/",
+      "**/.tubo/",
+      "**/next-env.d.ts",
+    ],
     rules: {
       "unicorn/filename-case": [
         "off",
--- apps/catalog/eslint.config.mjs
+++ apps/catalog/eslint.config.mjs
@@ -1,12 +1,14 @@
 // For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
 import storybook from "eslint-plugin-storybook";
-
+import { defineConfig } from "eslint/config";
 import base from "@casaub0n/eslint-base";
 
-export default [
+export default defineConfig([
+  {
+    ignores: ["**/eslint.config.mjs"],
+  },
   ...base({
     tsConfigurationRootDirectory: import.meta.dirname,
-    tsconfigFileName: "./tsconfig.json",
   }),
   ...storybook.configs["flat/recommended"],
-];
+]);
