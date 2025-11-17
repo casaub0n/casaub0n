9f370367343d29cd9761b90bd0a4b1329759139a
Casaubon <netzach.yoshida@gmail.com>
2025-11-17 11:21:11 +0900

    fix: 🐛 eslint (#5482)

--- packages/parse-html/eslint.config.mjs
+++ packages/parse-html/eslint.config.mjs
@@ -2,8 +2,10 @@ import { defineConfig } from "eslint/config";
 import base from "@casaub0n/eslint-base";
 
 export default defineConfig([
+  {
+    ignores: ["**/eslint.config.mjs"],
+  },
   ...base({
     tsConfigurationRootDirectory: import.meta.dirname,
-    tsconfigFileName: "./tsconfig.json",
   }),
 ]);
