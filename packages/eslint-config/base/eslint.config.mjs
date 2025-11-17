f41e7dd4b1228db9c3d3e3b68f2254684088da4c
Casaubon <netzach.yoshida@gmail.com>
2025-11-17 13:30:39 +0900

    fix eslint config (#5485)
    * fix: 🐛 eslint
    
    * fix: 🐛 eslint
    
    config

--- packages/eslint-config/base/eslint.config.mjs
+++ packages/eslint-config/base/eslint.config.mjs
@@ -2,8 +2,10 @@ import { defineConfig } from "eslint/config";
 import base from "./dist/index.mjs";
 
 export default defineConfig([
+  {
+    ignores: ["**/eslint.config.mjs"],
+  },
   ...base({
     tsConfigurationRootDirectory: import.meta.dirname,
-    tsconfigFileName: "./tsconfig.json",
   }),
 ]);
--- packages/ideal-addon/eslint.config.mjs
+++ packages/ideal-addon/eslint.config.mjs
@@ -1,10 +1,12 @@
-import base from "@casaub0n/eslint-base";
 import { defineConfig } from "eslint/config";
+import base from "@casaub0n/eslint-base";
 
 export default defineConfig([
+  {
+    ignores: ["**/eslint.config.mjs"],
+  },
   ...base({
     tsConfigurationRootDirectory: import.meta.dirname,
-    tsconfigFileName: "./tsconfig.json",
   }),
   {
     rules: {
