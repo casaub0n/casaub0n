e81b2f8ba330aba126d886639ca81802eeba9163
Casaubon <netzach.yoshida@gmail.com>
2025-11-24 16:22:47 +0900

    fix: 🐛 tsconfig

--- packages/ui/eslint.config.mjs
+++ packages/ui/eslint.config.mjs
@@ -2,9 +2,11 @@ import { defineConfig } from "eslint/config";
 import base from "@casaub0n/eslint-config-react";
 
 export default defineConfig([
+  {
+    ignores: ["**/eslint.config.mjs"],
+  },
   ...base({
     tsConfigurationRootDirectory: import.meta.dirname,
-    tsconfigFileName: "./tsconfig.json",
   }),
   {
     rules: {
