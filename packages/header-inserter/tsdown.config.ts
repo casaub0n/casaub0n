f9921b14d3970908bc695ab88150b29886442230
Casaubon <netzach.yoshida@gmail.com>
2025-12-15 18:56:11 +0900

    feat: 🎸 use tsdown instead of tsup(#5703)
    remove old configs, which is tsup and esbuild. lint check first-line code

---
+++ packages/header-inserter/tsdown.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from "tsdown";
+
+export default defineConfig({
+  entry: ["./src/index.ts"],
+  shims: true,
+  dts: true,
+  clean: true,
+  format: ["esm"],
+  target: "esnext",
+  platform: "node",
+  skipNodeModulesBundle: true,
+  // eslint-disable-next-line unicorn/better-regex
+  noExternal: [/(?:.*)/],
+});
