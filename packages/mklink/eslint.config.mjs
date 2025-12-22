559e1131307706897c494174d2a8f364dccddf58
Casaubon <netzach.yoshida@gmail.com>
2025-12-22 17:48:35 +0900

    fix: 🐛 eslint config in mdlink

--- packages/mklink/eslint.config.mjs
+++ packages/mklink/eslint.config.mjs
@@ -1,14 +1,13 @@
 import { defineConfig } from "eslint/config";
 import base from "@casaub0n/eslint-base";
-import { off } from "process";
 
 export default defineConfig([
   {
     ignores: ["**/eslint.config.mjs"],
     rules: {
-      "@typescript-eslint/explicit-function-return-type": off,
-      "no-console": off,
-      "@typescript-eslint/no-dynamic-delete": off,
+      "@typescript-eslint/explicit-function-return-type": "off",
+      "no-console": "off",
+      "@typescript-eslint/no-dynamic-delete": "off",
     },
   },
   ...base({
