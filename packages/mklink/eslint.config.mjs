8ca0ba925558255e9382c8535885bb65954643a0
Casaubon <netzach.yoshida@gmail.com>
2025-12-22 18:36:38 +0900

    fix: 🐛 lint (#5767)

--- packages/mklink/eslint.config.mjs
+++ packages/mklink/eslint.config.mjs
@@ -2,14 +2,15 @@ import { defineConfig } from "eslint/config";
 import base from "@casaub0n/eslint-base";
 
 export default defineConfig([
+  {
+    ignores: ["dist/*", "eslint.config.mjs"],
+  },
   ...base({
     tsConfigurationRootDirectory: import.meta.dirname,
   }),
   {
-    ignores: ["**/eslint.config.mjs"],
     rules: {
       "@typescript-eslint/explicit-function-return-type": "off",
-      "no-console": "off",
       "@typescript-eslint/no-dynamic-delete": "off",
     },
   },
