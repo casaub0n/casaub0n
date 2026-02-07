a5a2ec1647765cd432da1b7ef05230d4cad89add
Casaubon <netzach.yoshida@gmail.com>
2026-02-07 19:39:26 +0900

    fix: 🐛 eslint configs
    for ui project

--- packages/eslint-config/react/src/index.ts
+++ packages/eslint-config/react/src/index.ts
@@ -124,6 +124,29 @@ const config = ({
         ...eslintPluginUnicorn.configs.all.rules,
         ...importX.flatConfigs.recommended.rules,
         ...importX.flatConfigs.typescript.rules,
+
+        // It is used ui package rule
+        "@typescript-eslint/explicit-function-return-type": "off",
+        /**
+         * [eslint-plugin-unicorn/docs/rules/prevent-abbreviations.md at main · sindresorhus/eslint-plugin-unicorn](https://github.com/sindresorhus/eslint-plugin-unicorn/blob/main/docs/rules/prevent-abbreviations.md#options)
+         */
+        "unicorn/prevent-abbreviations": [
+          "error",
+          {
+            replacements: {
+              props: {
+                properties: false,
+              },
+              utils: {
+                utilities: false,
+              },
+            },
+          },
+        ],
+        /**
+         * [eslint-plugin-unicorn/docs/rules/no-keyword-prefix.md at v60.0.0 · sindresorhus/eslint-plugin-unicorn](https://github.com/sindresorhus/eslint-plugin-unicorn/blob/v60.0.0/docs/rules/no-keyword-prefix.md#disallowedprefixes)
+         */
+        "unicorn/no-keyword-prefix": ["error", { disallowedPrefixes: ["className"] }],
       },
       settings: {
         "import-x/resolver-next": createTypeScriptImportResolver({
--- packages/ui/eslint.config.mjs
+++ packages/ui/eslint.config.mjs
@@ -8,29 +8,4 @@ export default defineConfig([
   ...base({
     tsConfigurationRootDirectory: import.meta.dirname,
   }),
-  {
-    rules: {
-      "@typescript-eslint/explicit-function-return-type": "off",
-      /**
-       * [eslint-plugin-unicorn/docs/rules/prevent-abbreviations.md at main · sindresorhus/eslint-plugin-unicorn](https://github.com/sindresorhus/eslint-plugin-unicorn/blob/main/docs/rules/prevent-abbreviations.md#options)
-       */
-      "unicorn/prevent-abbreviations": [
-        "error",
-        {
-          replacements: {
-            props: {
-              properties: false,
-            },
-            utils: {
-              utilities: false,
-            },
-          },
-        },
-      ],
-      /**
-       * [eslint-plugin-unicorn/docs/rules/no-keyword-prefix.md at v60.0.0 · sindresorhus/eslint-plugin-unicorn](https://github.com/sindresorhus/eslint-plugin-unicorn/blob/v60.0.0/docs/rules/no-keyword-prefix.md#disallowedprefixes)
-       */
-      "unicorn/no-keyword-prefix": ["error", { disallowedPrefixes: ["className"] }],
-    },
-  },
 ]);
