a4436187684be9e4443097d2a4a406fca5afae2c
Casaubon <netzach.yoshida@gmail.com>
2026-03-02 20:34:44 +0900

    feat: 🎸 generate eslint config rules types (#6196)
    use eslint-typegen

--- packages/eslint-config/base/eslint.config.mjs
+++ packages/eslint-config/base/eslint.config.mjs
@@ -3,7 +3,7 @@ import base from "./dist/index.mjs";
 
 export default defineConfig([
   {
-    ignores: ["**/eslint.config.mjs"],
+    ignores: ["**/eslint.config.mjs", "./src/types.gen.d.ts"],
   },
   ...base({
     tsConfigurationRootDirectory: import.meta.dirname,
