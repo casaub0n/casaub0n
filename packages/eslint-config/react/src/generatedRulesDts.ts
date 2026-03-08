1752c34f02f1c1369b9a0fc9329db66221e8d8f4
Casaubon <netzach.yoshida@gmail.com>
2026-03-08 21:14:31 +0900

    Create generatedRulesDts.ts for ESLint plugin definitions
    This file generates TypeScript definitions for various ESLint plugins and writes them to src/types.gen.d.ts.

---
+++ packages/eslint-config/react/src/generatedRulesDts.ts
@@ -0,0 +1,27 @@
+import fs from "node:fs/promises";
+import { pluginsToRulesDTS } from "eslint-typegen/core";
+import pluginReact from "eslint-plugin-react";
+import eslintPluginUnicorn from "eslint-plugin-unicorn";
+import unusedImports from "eslint-plugin-unused-imports";
+import turboPlugin from "eslint-plugin-turbo";
+import pluginReactHooks from "eslint-plugin-react-hooks";
+import { importX } from "eslint-plugin-import-x";
+import pluginTs from "@typescript-eslint/eslint-plugin";
+
+export const generatedRulesDts = async () => {
+  const dts = await pluginsToRulesDTS({
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    "@typescript-eslint": pluginTs as any,
+    "unused-imports": unusedImports,
+    unicorn: eslintPluginUnicorn,
+    turbo: turboPlugin,
+    react: pluginReact,
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    "react-hooks": pluginReactHooks as any,
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    "import-x": importX as any,
+  });
+  await fs.writeFile("src/types.gen.d.ts", dts);
+  // eslint-disable-next-line no-console
+  console.log("Generated src/types.gen.d.ts");
+};
