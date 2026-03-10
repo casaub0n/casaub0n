d619932b9b6d0234f1daf2b72749696a94e9aa3a
Casaubon <netzach.yoshida@gmail.com>
2026-03-10 17:53:49 +0900

    refactor: 💡 dry (#6260)

--- packages/eslint-config/react/tsdown.config.ts
+++ packages/eslint-config/react/tsdown.config.ts
@@ -1,13 +1,6 @@
 import { defineConfig } from "tsdown";
 import fs from "node:fs/promises";
 import { pluginsToRulesDTS } from "eslint-typegen/core";
-import pluginReact from "eslint-plugin-react";
-import eslintPluginUnicorn from "eslint-plugin-unicorn";
-import unusedImports from "eslint-plugin-unused-imports";
-import turboPlugin from "eslint-plugin-turbo";
-import pluginReactHooks from "eslint-plugin-react-hooks";
-import { importX } from "eslint-plugin-import-x";
-import pluginTs from "@typescript-eslint/eslint-plugin";
 import { consola } from "consola";
 
 export default defineConfig({
@@ -25,19 +18,8 @@ export default defineConfig({
   },
   hooks: {
     "build:prepare": async () => {
-      // must be direct import
-      const dts = await pluginsToRulesDTS({
-        // eslint-disable-next-line @typescript-eslint/no-explicit-any
-        "@typescript-eslint": pluginTs as any,
-        "unused-imports": unusedImports,
-        unicorn: eslintPluginUnicorn,
-        turbo: turboPlugin,
-        react: pluginReact,
-        // eslint-disable-next-line @typescript-eslint/no-explicit-any
-        "react-hooks": pluginReactHooks as any,
-        // eslint-disable-next-line @typescript-eslint/no-explicit-any
-        "import-x": importX as any,
-      });
+      const myPlugins = await import("./src/my-plugins.ts").then((m) => m.myPlugins);
+      const dts = await pluginsToRulesDTS(myPlugins);
       await fs.writeFile("src/types.gen.d.ts", dts);
       consola.log("Generated src/types.gen.d.ts");
     },
