653c6d30927d67a896ba1f83522ef37d71b07a47
Casaubon <netzach.yoshida@gmail.com>
2026-03-26 16:13:03 +0900

    fix: 🐛 tsconfig (#6423)

--- packages/ideal-addon/src/popup.ts
+++ packages/ideal-addon/src/popup.ts
@@ -4,11 +4,15 @@ import type { MessageActionsId, ResponseMessageData, ZennArticleData } from "./t
 document.addEventListener("DOMContentLoaded", () => {
   // 現在のタブ情報を取得
   chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-    if (tabs.length === 0 || tabs[0].id === undefined) {
+    const tabs0 = tabs[0];
+    if (tabs0 === undefined) {
+      throw new Error("アクティブなタブが見つかりませんでした。");
+    }
+    if (tabs.length === 0 || tabs0.id === undefined) {
       throw new Error("アクティブなタブが見つかりませんでした。");
     }
     chrome.tabs.sendMessage<MessageActionsId>(
-      tabs[0].id,
+      tabs0.id,
       { action: "get-zenn-articles" },
       (response: ResponseMessageData | undefined) => {
         if (chrome.runtime.lastError) {
