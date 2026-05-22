95af5c9722f60554f225b937413118aa4dd7c492
Casaubon <netzach.yoshida@gmail.com>
2026-05-23 05:34:47 +0900

    fix: 🐛 hotfix

--- apps/casaub0n-page/src/app/page.tsx
+++ apps/casaub0n-page/src/app/page.tsx
@@ -1,5 +1,13 @@
 import { Top } from "@/components/templates/Top";
+import { layoutMetadata } from "@/components/layouts/layoutMetadata";
+
+// eslint-disable-next-line unicorn/prefer-export-from
+export const metadata = layoutMetadata;
 
 export default function Page() {
-  return <Top />;
+  return (
+    <>
+      <Top />
+    </>
+  )
 }
