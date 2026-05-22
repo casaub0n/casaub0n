71cade0173c00a414ec5abbb32c88e48bf9f8c0c
Casaubon <netzach.yoshida@gmail.com>
2026-05-23 05:51:34 +0900

    fix: 🐛 hotfix

--- apps/casaub0n-page/src/app/layout.tsx
+++ apps/casaub0n-page/src/app/layout.tsx
@@ -4,14 +4,15 @@ import type { ReactNode } from "react";
 import { Inter } from "next/font/google";
 import Script from "next/script";
 
-// import { layoutMetadata } from "@/components/layouts/layoutMetadata";
+import { layoutMetadata } from "@/components/layouts/layoutMetadata";
 
 import type { Article, WithContext } from "schema-dts";
 
 
 const inter = Inter({ subsets: ["latin"] });
 
-// const metadata = layoutMetadata;
+// eslint-disable-next-line unicorn/prefer-export-from
+export const metadata = layoutMetadata;
 
 /**
  * @see https://nextjs.org/docs/app/building-your-application/optimizing/metadata#json-ld
--- apps/casaub0n-page/src/app/page.tsx
+++ apps/casaub0n-page/src/app/page.tsx
@@ -1,15 +1,7 @@
 "use client";
 
 import { Top } from "@/components/templates/Top";
-// import { layoutMetadata } from "@/components/layouts/layoutMetadata";
-
- 
-// export const metadata = layoutMetadata;
 
 export default function Page() {
-  return (
-    <>
-      <Top />
-    </>
-  )
+  return <Top />
 }
