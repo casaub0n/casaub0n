144da0720275bcfda0f04c47121350a8f8c17394
Casaubon <netzach.yoshida@gmail.com>
2026-07-10 16:48:18 +0900

    fix: 🐛 eslint rules

--- apps/casaub0n-astro/eslint.config.mjs
+++ apps/casaub0n-astro/eslint.config.mjs
@@ -1,4 +1,21 @@
 import { defineConfig } from "eslint/config";
 import base from "@casaub0n/eslint-config-astro";
 
-export default defineConfig([...base]);
+export default defineConfig([
+  ...base,
+  {
+    ignores: ["**/eslint.config.mjs", "**/env.d.ts"],
+    rules: {
+      "unicorn/prevent-abbreviations": "off",
+      "unicorn/name-replacements": [
+        "error",
+        {
+          allowList: {
+            props: true,
+            Props: true,
+          },
+        },
+      ],
+    },
+  },
+]);
