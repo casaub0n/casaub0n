1a0504317d820f8391ddb2e419df721f03fd19a8
Casaubon <netzach.yoshida@gmail.com>
2026-07-10 16:02:15 +0900

    fix: 🐛 unicorn

--- packages/eslint-config/astro/eslint.config.mjs
+++ packages/eslint-config/astro/eslint.config.mjs
@@ -4,6 +4,9 @@ import base from "@casaub0n/eslint-base";
 export default defineConfig([
   {
     ignores: ["**/eslint.config.mjs"],
+    rules: {
+      "unicorn/prevent-abbreviations": "off",
+    },
   },
   ...base({
     tsConfigurationRootDirectory: import.meta.dirname,
