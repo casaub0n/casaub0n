2a1fbc72ff6485182a3ccf1231f7e3122bd28e91
Casaubon <netzach.yoshida@gmail.com>
2026-07-10 16:05:06 +0900

    fix: 🐛 eslint rules

--- packages/eslint-config/astro/eslint.config.mjs
+++ packages/eslint-config/astro/eslint.config.mjs
@@ -4,16 +4,8 @@ import base from "@casaub0n/eslint-base";
 export default defineConfig([
   {
     ignores: ["**/eslint.config.mjs"],
-    rules: {
-      "unicorn/prevent-abbreviations": "off",
-    },
   },
   ...base({
     tsConfigurationRootDirectory: import.meta.dirname,
   }),
-  {
-    rules: {
-      "unicorn/prevent-abbreviations": "off",
-    },
-  },
 ]);
