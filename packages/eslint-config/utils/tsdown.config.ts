54001c98da054af8dcd9e96bbdd3efddf6b789f4
Casaubon <netzach.yoshida@gmail.com>
2026-07-23 21:52:20 +0900

    fix: 🐛 tsdown configs (#6958)

--- packages/eslint-config/utils/tsdown.config.ts
+++ packages/eslint-config/utils/tsdown.config.ts
@@ -17,9 +17,9 @@ export default defineConfig({
   target: "esnext",
   platform: "node",
   minify: true,
-  // https://tsdown.dev/options/dependencies#deps-skipnodemodulesbundle
+  // https://tsdown.dev/options/dependencies#deps-neverBundle
   deps: {
-    skipNodeModulesBundle: true,
+    neverBundle: true,
   },
   hooks: {
     "build:prepare": async () => {
